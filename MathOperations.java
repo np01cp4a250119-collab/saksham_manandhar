af3c8fb538e8a71b569da0a1d20ac9010a4a6e6d
saksham manandhar <sakshammanandhar2007@gmail.com>
2025-11-20 16:21:10 +0545

    mayhoperations done

---
+++ MathOperations.java
@@ -0,0 +1,31 @@
+public class MathOperations{
+    public static void main(String[] args){
+        System.out.println("ARTHEMATIC OPERAORS");
+        int a = 10;
+        int b =5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+        
+        System.out.println("Unary/post-increment and pre-increment operators/");
+        
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println (--b);
+        System.out.println (b--);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println (a==b);
+        System.out.println (a!=b);
+        System.out.println (a>=b);
+        System.out.println (a<=b);
+          
+        int c=(a>b) ?a:b;
+        System.out. println (c);
+        
+        
+    }
+}
