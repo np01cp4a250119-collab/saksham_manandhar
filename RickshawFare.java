74c8373099c7159857847d2ced5e4c19f2f04e89
saksham manandhar <sakshammanandhar2007@gmail.com>
2025-11-20 22:13:20 +0545

    senario answer

---
+++ RickshawFare.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class RickshawFare {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter distance (in km): ");
+        double distance = sc.nextDouble();
+
+        System.out.print("Enter time (in minutes): ");
+        int time = sc.nextInt();
+
+        System.out.print("Are you local? (yes/no): ");
+        String isLocal = sc.next();
+
+        System.out.print("night time? (yes/no): ");
+        String isNight = sc.next();
+
+        
+        double baseFare = 50;     
+        double perKm = 20 * distance; 
+        double perMin = 2 * time;    
+
+        double total = baseFare + perKm + perMin;
+
+        
+        double localDiscount = isLocal.equalsIgnoreCase("yes") ? 0.10 : 0.0;
+        if (distance >= 10) {
+            total = total - (total * localDiscount);
+        }
+
+        
+        double nightCharge = isNight.equalsIgnoreCase("yes") ? 0.20 : 0.0;
+        total = total + (total * nightCharge);
+
+        
+        System.out.println("Total Fare: Rs. " + (int)total);
+        
+    }
+}
