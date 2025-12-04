32afa20cb1725891ebc94703feba2b31a4cf351f
saksham manandhar <sakshammanandhar2007@gmail.com>
2025-12-04 16:23:59 +0545

    new w5

---
+++ SumOfSquares.java
@@ -0,0 +1,13 @@
+public class SumOfSquares {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+
+        do {
+            sum +=i*i;
+            i++;
+        } while (i <= 10);
+
+        System.out.println(" sum is: " + sum);
+    }
+}
---
+++ w5Q5.java
@@ -0,0 +1,16 @@
+public class w5Q5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+
+            for (int s = 1; s <= 6 - i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
